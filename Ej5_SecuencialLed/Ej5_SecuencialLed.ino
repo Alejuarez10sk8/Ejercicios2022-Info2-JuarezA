a9c8b983396a1e0e90b17afb3dfa4a3462d82550
Ale Juarez <alejuarez10sk8@gmail.com>
2022-04-13 00:39:46 -0300

    ej5 agregado

---
+++ Ej5_SecuencialLed/Ej5_SecuencialLed.ino
@@ -0,0 +1,50 @@
+int buttonPin=2;
+volatile int ISRCounter = 3;
+long timeCounter = 0;
+const int timeThreshold = 140;
+int buttonState;
+int  T = 20;
+         
+void setup()       
+{ 
+         for (int i = 4; i < 12; i++){ 
+         pinMode (i, OUTPUT); }
+ attachInterrupt(digitalPinToInterrupt(2), debounce, RISING);
+ attachInterrupt(digitalPinToInterrupt(3), debounce1, RISING);
+} 
+void loop ()   
+{ 
+  if(ISRCounter==0){
+          for (int i = 4; i < 12; i++)
+{ 
+digitalWrite(i , HIGH) ; 
+delay (T); 
+digitalWrite (i, LOW); 
+delay (T);
+} 
+  }
+  if(ISRCounter==1){
+          for (int i = 10; i > 5; i=i-1)   
+{ 
+digitalWrite(i , HIGH) ; 
+delay (T); 
+digitalWrite (i, LOW); 
+delay (T);}
+}                
+} 
+ void debounce()
+{
+  if (millis() > timeCounter + timeThreshold)
+   {
+      ISRCounter=0;
+      timeCounter = millis();
+   }
+}
+ void debounce1()
+{
+  if (millis() > timeCounter + timeThreshold)
+   {
+      ISRCounter=1;
+      timeCounter = millis();
+   }
+}
